43306b8e30f975f55ea2bc030ca7dbd1f139b522
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-12 19:23:41 +0330

    feat: create `Document` model for sample Wikipedia website content"

---
+++ src/main/kotlin/lucene/constants/Fields.kt
@@ -0,0 +1,8 @@
+package com.webindexer.lucene.constants
+
+object Fields {
+    const val URL: String = "url"
+    const val TITLE: String = "title"
+    const val CONTENT: String = "content"
+    const val INFO_BOX: String = "info_box"
+}
---
+++ src/main/kotlin/lucene/models/DocumentModel.kt
@@ -0,0 +1,25 @@
+package com.webindexer.lucene.models
+
+import com.webindexer.lucene.constants.Fields
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.StringField
+import org.apache.lucene.document.TextField
+
+data class DocumentModel(
+    val url: String,
+    val title: String,
+    val content: String,
+    val infoBox: String,
+) {
+    fun toLuceneDocument(): Document {
+        val doc = Document()
+
+        doc.add(StringField(Fields.URL, url, Field.Store.YES))
+        doc.add(TextField(Fields.TITLE, title, Field.Store.YES))
+        doc.add(TextField(Fields.CONTENT, content, Field.Store.YES))
+        doc.add(TextField(Fields.INFO_BOX, infoBox, Field.Store.YES))
+
+        return doc
+    }
+}
