255885d2878319ab71758114d8d5fb3421bbbec5
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-12 16:39:26 +0330

    chore: add `ktor`, `skrapeit`, `lucene core` dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // Package(ktor): Build user friendly API
+    implementation("io.ktor:ktor-server-core-jvm:2.3.7")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.7")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.3.7")
+    implementation("io.ktor:ktor-server-default-headers-jvm:2.3.7")
+    // Package(skrapeit): Extract webpage data from fetched HTML
+    implementation("it.skrape:skrapeit:1.1.5")
+    // Package(lucene core): Index documents for searching
+    implementation("org.apache.lucene:lucene-core:9.9.1")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
 }
