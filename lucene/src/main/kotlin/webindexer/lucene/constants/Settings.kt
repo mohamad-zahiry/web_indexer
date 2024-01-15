bb8142ef4f96bf01c61e3859182ebf9e12093c3e
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-15 21:54:57 +0330

    feat: change lucene default path

--- lucene/src/main/kotlin/webindexer/lucene/constants/Settings.kt
+++ lucene/src/main/kotlin/webindexer/lucene/constants/Settings.kt
@@ -1,6 +1,6 @@
 package webindexer.lucene.constants
 
 object Settings {
-    const val INDEX_DIR_PATH: String = ""
+    const val INDEX_DIR_PATH: String = "lucene_index_default_path"
     const val MAX_SEARCH_RESULT: Int = 10
 }
