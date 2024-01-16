ec6cb6808fcd148896c811f575890ef3db95df9b
Mohamad Zahiri <mamadzhri@gamil.com>
2024-01-16 12:05:58 +0330

    feat: run api for test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,10 @@
 package webindexer
+
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+import webindexer.api.urlPatterns
+
+fun main() {
+    // Start server
+    urlPatterns.asServer(SunHttp(8080)).start()
+}
